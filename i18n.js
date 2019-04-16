1184d0bfc758a4f4f22733809f326248d25190d6
Itay Kimia <ikimia@gmail.com>
2019-04-16 14:21:02 +0300

    Remove languageDetector

--- i18n.js
+++ i18n.js
@@ -2,92 +2,82 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { getLocales } from "react-native-localize";
 
-const languageDetector = {
-  type: "languageDetector",
-  async: false,
-  init: () => {},
-  detect: () => getLocales()[0].languageCode,
-  cacheUserLanguage: () => {}
-};
-
-i18n
-  .use(languageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: "en",
-    resources: {
-      en: {
-        pay: {
-          pay: "Pay",
-          proceed: "Proceed",
-          receiptNumber: "Receipt Number",
-          makePaymentFor: "Make payment for",
-          makePayment: "Make Payment"
-        },
-        common: {
-          cancel: "Cancel",
-          split: "Split",
-          addTip: "Add tip",
-          description: "Description",
-          count: "Count",
-          price: "Price",
-          amount: "Amount",
-          taxes: "Taxes",
-          back: "Back"
-        },
-        purchases: {
-          purchases: "Purchases"
-        },
-        settings: {
-          settings: "Settings",
-          myAccount: "My Account",
-          paymentSettings: "Payment Settings",
-          language: "Language",
-          chooseLanguage: "Choose Language",
-          en: "English",
-          he: "Hebrew"
-        }
+i18n.use(initReactI18next).init({
+  lng: getLocales()[0].languageCode,
+  fallbackLng: "en",
+  resources: {
+    en: {
+      pay: {
+        pay: "Pay",
+        proceed: "Proceed",
+        receiptNumber: "Receipt Number",
+        makePaymentFor: "Make payment for",
+        makePayment: "Make Payment"
       },
-      he: {
-        pay: {
-          pay: "תשלום",
-          proceed: "המשך",
-          receiptNumber: "מספר חשבונית",
-          makePaymentFor: "תשלום עבור",
-          makePayment: "בצע תשלום"
-        },
-        common: {
-          cancel: "ביטול",
-          split: "פיצול",
-          addTip: "הוסף טיפ",
-          description: "תיאור",
-          count: "כמות",
-          price: "מחיר",
-          amount: 'סה"כ',
-          taxes: "מסים",
-          back: "חזור"
-        },
-        purchases: {
-          purchases: "רכישות"
-        },
-        settings: {
-          settings: "הגדרות",
-          myAccount: "החשבון שלי",
-          paymentSettings: "הגדרות תשלום",
-          language: "שפה",
-          chooseLanguage: "בחר שפה",
-          en: "אנגלית",
-          he: "עברית"
-        }
+      common: {
+        cancel: "Cancel",
+        split: "Split",
+        addTip: "Add tip",
+        description: "Description",
+        count: "Count",
+        price: "Price",
+        amount: "Amount",
+        taxes: "Taxes",
+        back: "Back"
+      },
+      purchases: {
+        purchases: "Purchases"
+      },
+      settings: {
+        settings: "Settings",
+        myAccount: "My Account",
+        paymentSettings: "Payment Settings",
+        language: "Language",
+        chooseLanguage: "Choose Language",
+        en: "English",
+        he: "Hebrew"
       }
     },
-
-    ns: ["common"],
-    defaultNS: "common",
-    debug: false,
-    interpolation: {
-      escapeValue: false
+    he: {
+      pay: {
+        pay: "תשלום",
+        proceed: "המשך",
+        receiptNumber: "מספר חשבונית",
+        makePaymentFor: "תשלום עבור",
+        makePayment: "בצע תשלום"
+      },
+      common: {
+        cancel: "ביטול",
+        split: "פיצול",
+        addTip: "הוסף טיפ",
+        description: "תיאור",
+        count: "כמות",
+        price: "מחיר",
+        amount: 'סה"כ',
+        taxes: "מסים",
+        back: "חזור"
+      },
+      purchases: {
+        purchases: "רכישות"
+      },
+      settings: {
+        settings: "הגדרות",
+        myAccount: "החשבון שלי",
+        paymentSettings: "הגדרות תשלום",
+        language: "שפה",
+        chooseLanguage: "בחר שפה",
+        en: "אנגלית",
+        he: "עברית"
+      }
     }
-  });
+  },
+
+  ns: ["common"],
+  defaultNS: "common",
+  debug: false,
+  interpolation: {
+    escapeValue: false
+  }
+});
 
 export default i18n;
