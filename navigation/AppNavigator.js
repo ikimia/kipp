188c89d52014fc15eb668842d6d365cff1360536
Itay Kimia <ikimia@gmail.com>
2019-04-16 14:58:37 +0300

    Remove WrappedStack

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,16 +1,5 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
 import MainTabNavigator from "./MainTabNavigator";
 
-const Stack = createSwitchNavigator({ MainTabNavigator });
-
-function WrappedStack(props) {
-  const { t } = useTranslation("common");
-  return <Stack screenProps={{ t }} {...props} />;
-}
-WrappedStack.router = Stack.router;
-
-export default createAppContainer(WrappedStack);
+export default createAppContainer(createSwitchNavigator({ MainTabNavigator }));
