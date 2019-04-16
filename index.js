004141c123b0877f25c57eec53f0bccaa8b822bd
Itay Kimia <ikimia@gmail.com>
2019-04-16 15:19:18 +0300

    Add use direction

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
+
+// eslint-disable-next-line no-unused-vars
+import i18n from "./i18n";
 
 AppRegistry.registerComponent(appName, () => App);
