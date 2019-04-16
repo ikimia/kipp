63f9c5667563e021ceac0cb4bc40643712d2f9cf
Itay Kimia <ikimia@gmail.com>
2019-04-16 20:08:03 +0300

    Add missing direction function

--- hooks/direction.js
+++ hooks/direction.js
@@ -1,9 +1,14 @@
 import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+
+const isLanguageRTL = language => language.startsWith("he");
 
 export const RTL = "rtl";
 export const LTR = "ltr";
 
 export function useDirection() {
   const { i18n } = useTranslation();
-  return i18n.language.startsWith("he") ? RTL : LTR;
+  return isLanguageRTL(i18n.language) ? RTL : LTR;
 }
+
+export const isRTL = () => isLanguageRTL(i18next.language);
