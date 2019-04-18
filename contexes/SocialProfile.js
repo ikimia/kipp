381e0237b0c31f17a0b603b98900b0b738924d01
Moshe Zada <zada.moshe@gmail.com>
2019-04-18 13:44:07 +0300

    Add main facebook login screen and profile screen

---
+++ contexes/SocialProfile.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SocialProfile = React.createContext({
+  setUserProfile: () => {},
+  logOut: () => {},
+  userProfile: {
+    isLoggedIn: false
+  }
+});
---
+++ screens/SocialProfileScreen.js
@@ -0,0 +1,70 @@
+import React, { useContext } from "react";
+import { Image } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Title,
+  Right
+} from "native-base";
+import { SocialProfile } from "../contexes/SocialProfile";
+import { useTranslation } from "react-i18next";
+import BackButton from "../components/BackButton";
+
+export default function SocialProfileScreen() {
+  const { userProfile, logOut } = useContext(SocialProfile);
+  const { t } = useTranslation("socialNetworks");
+  return (
+    <Container>
+      <Header>
+        <Left>
+          <BackButton />
+        </Left>
+        <Body>
+          <Title>{t("socialProfile")}</Title>
+        </Body>
+        <Right />
+      </Header>
+      <Content>
+        <Card style={{ flex: 0 }}>
+          <CardItem>
+            <Left>
+              <Body>
+                <Text>{userProfile.name}</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Image
+                source={{ uri: userProfile.picture }}
+                style={{ height: 300, width: 300, flex: 1 }}
+              />
+            </Body>
+          </CardItem>
+          <CardItem>
+            <Left>
+              <Button
+                transparent
+                textStyle={{ color: "#87838B" }}
+                onPress={() => {
+                  logOut();
+                }}
+              >
+                <Icon name="log-out" />
+                <Text>{t("logout")}</Text>
+              </Button>
+            </Left>
+          </CardItem>
+        </Card>
+      </Content>
+    </Container>
+  );
+}
