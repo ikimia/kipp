3bd3c305a97d28ecc9967065e7d3b5ca5c11cc63
Itay Kimia <ikimia@gmail.com>
2019-04-18 18:51:21 +0300

    Put language loading back in App

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import { AppRegistry } from "react-native";
-import AsyncStorage from "@react-native-community/async-storage";
 import App from "./App";
 import { name as appName } from "./app.json";
+
+// eslint-disable-next-line no-unused-vars
 import i18n from "./i18n";
 
-AsyncStorage.getItem("@StreetPay_language").then(code => {
-  if (code) {
-    i18n.changeLanguage(code);
-  }
-  AppRegistry.registerComponent(appName, () => App);
-});
+AppRegistry.registerComponent(appName, () => App);
