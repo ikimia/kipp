303ef0e899cbefe3be5f9587f009a2aa8edcee53
Itay Kimia <ikimia@gmail.com>
2019-04-22 17:59:08 +0300

    Use Navigation from context

--- screens/PastOrderScreen.js
+++ screens/PastOrderScreen.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import OrderScreen from "./OrderScreen";
+import { NavigationContext } from "react-navigation";
 
-export default function PastOrderScreen({ navigation }) {
+export default function PastOrderScreen() {
+  const navigation = useContext(NavigationContext);
   const storeName = navigation.getParam("storeName");
   return (
     <OrderScreen hidePayment storeName={storeName} receiptNumber="12345" />
