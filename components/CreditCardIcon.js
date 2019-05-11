8614bf1319fa2effaa00b93ee4952075823b0441
Itay Kimia <ikimia@gmail.com>
2019-05-11 14:52:10 +0300

    Remove CreditCardFunctions

--- components/CreditCardIcon.js
+++ components/CreditCardIcon.js
@@ -1,6 +1,20 @@
 import * as React from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { getCreditCardIcon } from "../screens/CreditCardFunctions";
+
+const CC_TESTERS = [
+  [/^4/, "cc-visa"],
+  [/^5\d/, "cc-mastercard"],
+  [/^37/, "cc-amex"],
+  [/^36/, "cc-diners-club"]
+];
+function getCreditCardIcon(cardNumber) {
+  for (const [pattern, iconName] of CC_TESTERS) {
+    if (pattern.test(cardNumber)) {
+      return iconName;
+    }
+  }
+  return "credit-card-alt";
+}
 
 export default function CreditCardIcon({ cardNumber, style }) {
   return <Icon name={getCreditCardIcon(cardNumber)} style={style} />;
