30a49b3dfaf4b737090fb50cac3199d9f993f24e
Itay Kimia <ikimia@gmail.com>
2019-05-19 19:17:03 +0300

    Disallow SVG import

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,10 @@
-const { getDefaultConfig } = require("metro-config");
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false
-        }
-      })
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svgx"),
-      sourceExts: [...sourceExts, "svgx"]
-    }
-  };
-})();
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  }
+};
