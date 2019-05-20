f0804a17bec3254db253e9678734940d58221f3a
Itay Kimia <ikimia@gmail.com>
2019-05-20 17:43:42 +0300

    Import unnamed i18n

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
 
-// eslint-disable-next-line no-unused-vars
-import i18n from "./i18n";
+import "./i18n";
 
 AppRegistry.registerComponent(appName, () => App);
