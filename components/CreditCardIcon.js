ebfc603a4185848004255f98b26b3517d50466b3
Itay Kimia <ikimia@gmail.com>
2019-05-23 13:20:01 +0300

    Add CC to Settings Page

--- components/CreditCardIcon.js
+++ components/CreditCardIcon.js
@@ -1,21 +1,36 @@
 import * as React from "react";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { Image } from "react-native";
+
+const IMAGES = {
+  visa: require("../assets/img/visa.png"),
+  mastercard: require("../assets/img/mastercard.png"),
+  amex: require("../assets/img/amex.png"),
+  dinersclub: require("../assets/img/dinersclub.png"),
+  blank: require("../assets/img/blank.png")
+};
 
 const CC_TESTERS = [
-  [/^4/, "cc-visa"],
-  [/^5\d/, "cc-mastercard"],
-  [/^37/, "cc-amex"],
-  [/^36/, "cc-diners-club"]
+  [/^4/, IMAGES.visa],
+  [/^5\d/, IMAGES.mastercard],
+  [/^37/, IMAGES.amex],
+  [/^36/, IMAGES.dinersclub]
 ];
 function getCreditCardIcon(cardNumber) {
-  for (const [pattern, iconName] of CC_TESTERS) {
-    if (pattern.test(cardNumber)) {
-      return iconName;
+  if (cardNumber) {
+    for (const [pattern, source] of CC_TESTERS) {
+      if (pattern.test(cardNumber)) {
+        return source;
+      }
     }
   }
-  return "credit-card-alt";
+  return IMAGES.blank;
 }
 
 export default function CreditCardIcon({ cardNumber, style }) {
-  return <Icon name={getCreditCardIcon(cardNumber)} style={style} />;
+  return (
+    <Image
+      source={getCreditCardIcon(cardNumber)}
+      style={[{ width: 24, height: 15 }, style]}
+    />
+  );
 }
