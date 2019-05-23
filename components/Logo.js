8eb687a07221419645509c75f1589e0db43431ef
Itay Kimia <ikimia@gmail.com>
2019-05-23 15:55:30 +0300

    Text to StyledText

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import StyledText from "./StyledText";
 
 export default function Logo({ fontSize = 20, color }) {
   return (
     <View style={{ flexDirection: "row", alignItems: "center" }}>
-      <Text style={{ fontFamily: "Baumans", fontSize, color }}>Kipp</Text>
+      <StyledText fontFamily="Baumans" size={fontSize} color={color}>
+        Kipp
+      </StyledText>
     </View>
   );
 }
