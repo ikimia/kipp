c540a8695710d1c5f72cd2cd0ce1c89419be5674
Moshe Zada <zada.moshe@gmail.com>
2019-05-23 18:26:25 +0300

    Move serviceAccountKey.json to function dir

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
