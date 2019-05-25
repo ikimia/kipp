3e74b5bcefcc6a0117f5d887aa822144351a392a
Moshe Zada <zada.moshe@gmail.com>
2019-05-25 18:29:37 +0300

    Update screen name for analytics

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,5 +1,32 @@
+import React from "react";
+import firebase from "react-native-firebase";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
-
 import MainTabNavigator from "./MainTabNavigator";
 
-export default createAppContainer(createSwitchNavigator({ MainTabNavigator }));
+function getActiveRouteName(navigationState) {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  if (route.routes) {
+    return getActiveRouteName(route);
+  }
+  return route.routeName;
+}
+
+const AppContainer = createAppContainer(
+  createSwitchNavigator({ MainTabNavigator })
+);
+export default function AppNavigator() {
+  return (
+    <AppContainer
+      onNavigationStateChange={(prevState, currentState, action) => {
+        const currentScreen = getActiveRouteName(currentState);
+        const prevScreen = getActiveRouteName(prevState);
+        if (prevScreen !== currentScreen) {
+          firebase.analytics().setCurrentScreen(currentScreen);
+        }
+      }}
+    />
+  );
+}
