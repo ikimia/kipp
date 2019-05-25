00931c8bf94857505a8ac5d04e763030389393da
Moshe Zada <zada.moshe@gmail.com>
2019-05-25 20:15:30 +0300

    Add backdrop to login screen

--- components/Logo.js
+++ components/Logo.js
@@ -1,15 +1,12 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
-import { View } from "react-native";
 import StyledText from "./StyledText";
 
 export default function Logo({ fontSize = 20, color }) {
   return (
-    <View style={{ flexDirection: "row", alignItems: "center" }}>
-      <StyledText fontFamily="Baumans" size={fontSize} color={color}>
-        Kipp
-      </StyledText>
-    </View>
+    <StyledText fontFamily="Baumans" size={fontSize} color={color}>
+      Kipp
+    </StyledText>
   );
 }
 Logo.propTypes = {
