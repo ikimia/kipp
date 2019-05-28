bafc491560f51b88a2110322aa1b60f6fd310d0d
Itay Kimia <ikimia@gmail.com>
2019-05-28 15:49:45 +0300

    Move Firebase functions to Europe

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -12,6 +12,9 @@ admin.initializeApp({
   databaseURL: "https://academic-works-241411.firebaseio.com"
 });
 
+const defineFunction = handler =>
+  functions.region("europe-west1").https.onRequest(handler);
+
 function auth(next) {
   return async function(req, res) {
     const tokenId = req.get("Authorization").split("Bearer ")[1];
@@ -24,7 +27,7 @@ function auth(next) {
   };
 }
 
-exports.getCode = functions.https.onRequest(
+exports.getCode = defineFunction(
   auth((req, res) => {
     const code = Math.random()
       .toString(10)
@@ -37,7 +40,7 @@ async function charge(price, storeName, topic) {
   await admin.messaging().send({ topic, data: { price, storeName } });
 }
 
-exports.charge = functions.https.onRequest(async (req, res) => {
+exports.charge = defineFunction(async (req, res) => {
   const { paymentCode: topic, price, storeName } = req.body;
   await charge(price, storeName, topic);
   res.json({ topic });
@@ -52,7 +55,7 @@ exports.demo = functions.https.onRequest(async (req, res) => {
   res.send();
 });
 
-exports.acceptPayment = functions.https.onRequest(
+exports.acceptPayment = defineFunction(
   auth(async (req, res) => {
     const uid = req.user.uid;
     const { storeName, price } = req.body.data;
@@ -71,7 +74,7 @@ function repeat(num, func) {
     .map(() => func());
 }
 
-exports.getExploreData = functions.https.onRequest(
+exports.getExploreData = defineFunction(
   auth(async (req, res) => {
     const lanes = [
       ["Featured", repeat(5, getStoreName)],
@@ -85,7 +88,7 @@ exports.getExploreData = functions.https.onRequest(
   })
 );
 
-exports.getUserMemberships = functions.https.onRequest(
+exports.getUserMemberships = defineFunction(
   auth(async (req, res) => {
     res.json({ data: repeat(6, getStoreName) });
   })
