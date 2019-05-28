e3fc0b3f46b53ebe779ab4e563e98903da0d6a77
Itay Kimia <ikimia@gmail.com>
2019-05-28 18:44:41 +0300

    Remove PayConfirmScreen

--- screens/PayConfirmScreen.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import OrderScreen from "./OrderScreen";
-import { NavigationContext } from "react-navigation";
-
-export default function PayConfirmScreen() {
-  const navigation = useContext(NavigationContext);
-  const receiptNumber = navigation.getParam("receiptNumber", "NO-ID");
-  return <OrderScreen receiptNumber={receiptNumber} storeName="apparelStore" />;
-}
