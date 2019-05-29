f63a1ec5956660f0c8ebcc5e4d6661531efbe3b0
Itay Kimia <ikimia@gmail.com>
2019-05-29 13:41:44 +0300

    Add receipt sections

--- components/Purchases.js
+++ components/Purchases.js
@@ -1,12 +1,42 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, RefreshControl } from "react-native";
+import { View, RefreshControl, SectionList } from "react-native";
 import moment from "moment";
 import { useTranslation } from "react-i18next";
 import { NavigationContext } from "react-navigation";
-import { FlatList } from "react-native-gesture-handler";
 import ItemListItem, { COLORS } from "./ItemListItem";
 import StyledText from "./StyledText";
 import { getReceipts } from "../Backend";
+import groupBy from "lodash/groupBy";
+import ItemListSectionHeader from "./ItemListSectionHeader";
+
+const createSections = (() => {
+  const startOf = unitOfTime =>
+    moment()
+      .startOf(unitOfTime)
+      .valueOf();
+  const TODAY_LABEL = "Today";
+  const WEEK_LABEL = "This Week";
+  const MONTH_LABEL = "This Month";
+  const PAST_LABEL = "Past Purchases";
+  return receipts => {
+    const startOfDay = startOf("day");
+    const startOfWeek = startOf("week");
+    const startOfMonth = startOf("month");
+    const groups = groupBy(
+      receipts,
+      receipt =>
+        [TODAY_LABEL, WEEK_LABEL, MONTH_LABEL][
+          [startOfDay, startOfWeek, startOfMonth].findIndex(
+            time => receipt.created > time
+          )
+        ] || PAST_LABEL
+    );
+    const sections = [TODAY_LABEL, WEEK_LABEL, MONTH_LABEL, PAST_LABEL]
+      .filter(key => groups[key])
+      .map(key => ({ title: key, data: groups[key] }));
+    return sections;
+  };
+})();
 
 const m = (language, created) => {
   const localizedMoment = moment(created);
@@ -17,16 +47,23 @@ const m = (language, created) => {
 export default function Purchases() {
   const { navigate } = useContext(NavigationContext);
   const { t, i18n } = useTranslation("common");
-  const [data, setData] = useState([]);
+  const [sections, setSections] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
-  const refreshData = () => getReceipts().then(setData);
+  const refreshData = () => {
+    getReceipts()
+      .then(createSections)
+      .then(setSections);
+  };
   const { language } = i18n;
   useEffect(() => {
     refreshData();
   }, []);
   return (
-    <FlatList
-      data={data}
+    <SectionList
+      sections={sections}
+      renderSectionHeader={({ section: { title } }) => (
+        <ItemListSectionHeader title={title} />
+      )}
       refreshControl={
         <RefreshControl
           onRefresh={async () => {
