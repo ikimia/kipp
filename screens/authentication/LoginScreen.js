5670cb84e13434348f00abd57a64291d39ee4509
Itay Kimia <ikimia@gmail.com>
2019-06-07 21:45:35 +0300

    Refactor AuthenticationFlow

---
+++ screens/authentication/LoginScreen.js
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { useContext } from "react";
+import { View } from "react-native";
+import { SafeAreaView, NavigationContext } from "react-navigation";
+import { AccessToken } from "react-native-fbsdk";
+
+import Logo from "../../components/Logo";
+import FBLoginButton from "../../components/FBLoginButton";
+import Backdrop, { PATTERNS } from "../../components/Backdrop";
+import { signIn } from "../../Backend";
+import StyledText from "../../components/StyledText";
+import { RectButton, BorderlessButton } from "react-native-gesture-handler";
+
+export default function LoginScreen() {
+  const { navigate } = useContext(NavigationContext);
+  const login = async () => {
+    const accessToken = await AccessToken.getCurrentAccessToken();
+    if (!accessToken) {
+      return;
+    }
+    await signIn(accessToken.accessToken);
+    navigate("App");
+  };
+  return (
+    <SafeAreaView style={{ flex: 1, paddingHorizontal: 20 }}>
+      <Backdrop pattern={PATTERNS[1]} />
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <View style={{ alignItems: "center" }}>
+          <Logo fontSize={100} color="white" />
+        </View>
+        <View style={{ marginTop: 50 }}>
+          <FBLoginButton onLogin={login} />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 10
+            }}
+          >
+            <View
+              style={{ flex: 1, borderTopColor: "white", borderTopWidth: 1 }}
+            />
+            <View style={{ marginHorizontal: 10 }}>
+              <StyledText color="white">OR</StyledText>
+            </View>
+            <View
+              style={{ flex: 1, borderTopColor: "white", borderTopWidth: 1 }}
+            />
+          </View>
+          <RectButton
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 0.8)",
+              padding: 10,
+              borderRadius: 5,
+              alignItems: "center"
+            }}
+          >
+            <View>
+              <StyledText size={16}>Create an Account</StyledText>
+            </View>
+          </RectButton>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              marginTop: 10
+            }}
+          >
+            <StyledText color="white">Already have an account? </StyledText>
+            <BorderlessButton>
+              <StyledText color="white" bold>
+                Sign In
+              </StyledText>
+            </BorderlessButton>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ screens/authentication/TourScreen.js
@@ -0,0 +1,114 @@
+import * as React from "react";
+import { useContext, useState } from "react";
+import { View, StatusBar } from "react-native";
+import {
+  SafeAreaView,
+  NavigationContext,
+  NavigationEvents
+} from "react-navigation";
+
+import Logo from "../../components/Logo";
+import Backdrop, { PATTERNS } from "../../components/Backdrop";
+import StyledText from "../../components/StyledText";
+import { RectButton } from "react-native-gesture-handler";
+
+const TOUR = [
+  {
+    title: "Welcome to Kipp",
+    text: "Your life are about to change!\nContinue to know more."
+  },
+  {
+    title: "Say Goodbye to Your Wallet",
+    text:
+      "Kipp lets you pay without your wallet.\nOnce everything is set, your credit card will be stored on your device."
+  },
+  {
+    title: "Get Promotions You Want",
+    text:
+      "Based on what you buy, we will show you only the promotions that you are interested in.\nNo BS, we promise."
+  },
+  {
+    title: "Ready to Get Started?",
+    text: "Continue in order to sign in."
+  }
+];
+
+function Pagination({ size, index }) {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {[...Array(size)].map((_, i) => (
+        <View
+          key={i}
+          style={{
+            marginHorizontal: 5,
+            height: 10,
+            width: 10,
+            borderRadius: 5,
+            backgroundColor: "white",
+            opacity: i === index ? 1 : 0.2
+          }}
+        />
+      ))}
+    </View>
+  );
+}
+
+function ContinueButton({ onPress }) {
+  return (
+    <RectButton
+      style={{
+        borderColor: "white",
+        borderWidth: 2,
+        borderRadius: 25,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+      onPress={onPress}
+    >
+      <View>
+        <StyledText color="white" size={16} bold>
+          CONTINUE
+        </StyledText>
+      </View>
+    </RectButton>
+  );
+}
+
+export default function TourScreen() {
+  const { navigate } = useContext(NavigationContext);
+  const [activeSlide, setActiveSlide] = useState(0);
+  return (
+    <SafeAreaView style={{ flex: 1, paddingHorizontal: 20 }}>
+      <NavigationEvents
+        onWillFocus={() => {
+          StatusBar.setBarStyle("light-content");
+        }}
+      />
+      <Backdrop pattern={PATTERNS[1]} />
+      <View style={{ alignItems: "center" }}>
+        <Pagination size={TOUR.length} index={activeSlide} />
+      </View>
+      <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
+        <Logo fontSize={100} color="white" />
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <StyledText bold color="white" size={18} style={{ marginBottom: 15 }}>
+          {TOUR[activeSlide].title}
+        </StyledText>
+        <StyledText color="white" align="center">
+          {TOUR[activeSlide].text}
+        </StyledText>
+      </View>
+      <View style={{ marginVertical: 20 }}>
+        <ContinueButton
+          onPress={
+            activeSlide < TOUR.length - 1
+              ? () => setActiveSlide(n => n + 1)
+              : () => navigate("Login")
+          }
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
