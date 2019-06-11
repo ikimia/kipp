01e97df850e2d7c44cc402d5288ebe92d620706a
Moshe Zada <zada.moshe@gmail.com>
2019-06-11 12:02:17 +0300

    Revert "Add offers to main screen"
    This reverts commit 8ce42d256acff1005f141ec2afa3e3fc7e257319.

--- screens/OffersScreen.js
+++ screens/OffersScreen.js
@@ -1,14 +1,58 @@
 import * as React from "react";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { View, Image } from "react-native";
-import { RectButton, FlatList, TextInput } from "react-native-gesture-handler";
+import {
+  RectButton,
+  ScrollView,
+  FlatList,
+  TextInput
+} from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/Feather";
 import StyledText from "../components/StyledText";
 import AppHeader from "../components/AppHeader";
 import Container from "../components/Container";
 import StoreLogo from "../components/StoreLogo";
-import { NavigationContext, NavigationActions } from "react-navigation";
-import { OfferStorage } from "../Storage";
+
+function Chip({ title, icon }) {
+  return (
+    <RectButton
+      style={{
+        backgroundColor: "#EEE",
+        marginEnd: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 13
+      }}
+    >
+      <View
+        style={{ opacity: 0.6, flexDirection: "row", alignItems: "center" }}
+      >
+        {<Icon name={icon} style={{ marginEnd: 5 }} />}
+        <StyledText>{title}</StyledText>
+      </View>
+    </RectButton>
+  );
+}
+
+function Chips({ data }) {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={{ alignSelf: "stretch", paddingHorizontal: 10 }}
+    >
+      {data.map(({ icon, title }) => (
+        <Chip key={title} icon={icon} title={title} />
+      ))}
+    </ScrollView>
+  );
+}
+
+const FILTERS = [
+  { icon: "bookmark", title: "Saved" },
+  { icon: "navigation", title: "Near Me" },
+  { icon: "navigation", title: "Near Me" }
+];
 
 function SearchInput() {
   return (
@@ -63,16 +107,6 @@ async function getOffers() {
 
 export default function OffersScreen() {
   const [offers, setOffers] = useState([]);
-  const { dispatch } = useContext(NavigationContext);
-  const useOffer = offer => {
-    OfferStorage.set(offer);
-    dispatch(
-      NavigationActions.setParams({
-        key: "Pay",
-        params: { showNotification: true }
-      })
-    );
-  };
   useEffect(() => {
     getOffers().then(setOffers);
   }, []);
@@ -122,24 +156,18 @@ export default function OffersScreen() {
                   borderTopColor: "#CCC"
                 }}
               >
-                <RectButton style={{ flex: 1 }} onPress={() => useOffer(offer)}>
-                  <View
-                    style={{
-                      flex: 1,
-                      flexDirection: "row",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      padding: 10
-                    }}
-                  >
-                    <Icon
-                      name="plus-square"
-                      style={{ marginEnd: 5 }}
-                      size={14}
-                    />
-                    <StyledText bold>Use</StyledText>
-                  </View>
-                </RectButton>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: 10
+                  }}
+                >
+                  <Icon name="plus-square" style={{ marginEnd: 5 }} size={14} />
+                  <StyledText bold>Use</StyledText>
+                </View>
                 <View
                   style={{
                     flex: 1,
